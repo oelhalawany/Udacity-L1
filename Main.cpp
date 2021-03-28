6bb59b9844ce43f7742caa387ced79ad818cbf0a
oelhalawany <omar.elhalawany1@gmail.com>
2021-03-28 17:13:30 +0200

    Implemeted ExpandNeighbors

--- Main.cpp
+++ Main.cpp
@@ -140,13 +140,25 @@ void ExpandNeighbors(const vector<int>& current, int goal[2], vector<vector<int>
 {
 
     // TODO: Get current node's data.
+    int x1 = current[0];
+    int y1 = current[1];
+    int g1 = current[2];
 
-  // TODO: Loop through current node's potential neighbors.
-
-    // TODO: Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
-
-      // TODO: Increment g value, compute h value, and add neighbor to open list.
-
+    //Loop through current node's potential neighbors.
+    for (int i = 0; i < 4;i++)
+    {
+        int x2 = x1 + delta[i][0]; // 0,0 1,0 2,0 3,0
+        int y2 = y1 + delta[i][1]; // 0,1 1,1 2,1 3,1
+    
+        //Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
+        if (CheckValidCell(x2, y2, grid))
+        {
+            //Increment g value, compute h value, and add neighbor to open list.
+            int g2 = g1 + 1;
+            int h2 = Heuristic(x2, y2, goal[1], goal[2]);
+            AddToOpen(x2, y2, g2, h2, openlist, grid);
+        }
+    }
 }
 
 /**
