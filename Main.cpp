745a23dd8c9c01920d2f03c9f72988edba26d64b
oelhalawany <omar.elhalawany1@gmail.com>
2021-03-28 14:42:43 +0200

    Implemented Sort and Started Search function

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#include <algorithm>  // for sort
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
@@ -8,7 +9,7 @@ using std::vector;
 using std::cout;
 
 //Define ENUM class for states of the board cells
-enum class State { kEmpty, kObstacle };
+enum class State { kEmpty, kObstacle, kClosed, kPath};
 
 //Read cell state and return character 
 string CellString(State cell)
@@ -78,45 +79,92 @@ vector<vector<State>> ReadBoardFile(string path) {
 	return board;
 }
 
-/**
- * Implementation of A* search algorithm
- */
-vector<vector<State>> Search(vector<vector<State>> grid, int start[2], int goal[2])
-{
-
-	vector<vector<State>> solution{};
-
-	cout << "No path found! \n";
-
-	return solution;
-}
-
 // Calculate the manhattan distance
 int Heuristic(int x1, int y1, int x2, int y2) {
 	return (abs(x2 - x1) + abs(y2 - y1));
 }
 
-//Test Heuristic function
-void TestHeuristic() {
-	cout << "----------------------------------------------------------" << "\n";
-	cout << "Heuristic Function Test: ";
-	if (Heuristic(1, 2, 3, 4) != 4) {
-		cout << "failed" << "\n";
-		cout << "\n" << "Heuristic(1, 2, 3, 4) = " << Heuristic(1, 2, 3, 4) << "\n";
-		cout << "Correct result: 4" << "\n";
-		cout << "\n";
-	}
-	else if (Heuristic(2, -1, 4, -7) != 8) {
-		cout << "TestHeuristic Failed" << "\n";
-		cout << "\n" << "Heuristic(2, -1, 4, -7) = " << Heuristic(2, -1, 4, -7) << "\n";
-		cout << "Correct result: 8" << "\n";
-		cout << "\n";
-	}
-	else {
-		cout << "passed" << "\n";
-	}
-	cout << "----------------------------------------------------------" << "\n";
+/**
+ * Add a node to the open list and mark it as open.
+ */
+void AddToOpen(int x, int y, int g, int h, vector<vector<int>>& openNodes, vector<vector<State>>& grid)
+{
+	vector<int> node = { x,y,g,h };
+	openNodes.push_back(node);
+	grid[x][y] = State::kClosed;
 }
+
+/**
+ * Compare the F values of two cells.
+ */
+bool Compare(vector<int> node1, vector <int> node2)
+{
+	//Node formate is {x, y, g, h}
+
+	int fvalue_node1 = node1[2] + node1[3];
+	int fvalue_node2 = node2[2] + node2[3];
+
+	return fvalue_node1 > fvalue_node2;
+}
+
+/**
+ * Sort the two-dimensional vector of ints in descending order.
+ */
+void CellSort(vector<vector<int>>* v) {
+	sort(v->begin(), v->end(), Compare);
+}
+
+/**
+ * Implementation of A* search algorithm
+ */
+vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2])
+{
+	//Create a vector with solution of A* Search
+	vector<vector<State>> solution{};
+
+	//Create vector of open nodes
+	vector<vector<int>> open{};
+
+	// Initialize the starting node.
+	int x = init[0];
+	int y = init[1];
+	int g = 0;
+	int h = Heuristic(x, y, goal[0], goal[1]);
+	//List of open nodes and grid are passed by reference to be changed by AddToOpen function
+	AddToOpen(x, y, g, h, open, grid);
+
+	// TODO: while open vector is non empty 
+	while (open.empty() == false)
+	{
+		// TODO: Sort the open list using `CellSort`, and get the current node.
+		CellSort(&open);
+		// TODO: Get the x and y values from the current node,
+		auto current_node = open.back();
+		open.pop_back();
+		x = current_node[0];
+		y = current_node[1];
+
+		// and set grid[x][y] to kPath.
+		grid[x][y] = State::kPath;
+
+		// TODO: Check if you've reached the goal. If so, return grid.
+		if (x == goal[0] && y == goal[1])
+		{
+			return grid;
+		}
+		else
+		{
+			// If we're not done, expand search to current node's neighbors. This step will be completed in a later quiz.
+			// ExpandNeighbors
+		}
+	}
+
+	cout << "No path found! \n";
+	return solution;
+}
+
+
+#include "test.cpp"
 
 int main()
 {
@@ -139,4 +187,9 @@ int main()
 	//Print board
 	PrintBoard(solution);
 
+	/*** TESTS ***/
+	TestHeuristic();
+	TestAddToOpen();
+	TestCompare();
+	TestSearch();
 }
